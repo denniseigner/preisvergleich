925a89ea9a34db4bf0c1b13daf07dd59acc7ff09
Dennis Eigner <dennis@deigner.eu>
2024-05-02 18:06:15 +0200

    Part 4

--- webapp/manager/views.py
+++ webapp/manager/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Item
 
@@ -13,3 +14,18 @@ def index(request: HttpRequest) -> HttpResponse:
 def detail(request: HttpRequest, item_id: int) -> HttpResponse:
     item = get_object_or_404(Item, pk=item_id)
     return render(request, "detail.html", {"item": item})
+
+
+def editor(request: HttpRequest) -> HttpResponse:
+    return render(request, "editor.html")
+
+
+def create_new(request: HttpRequest) -> HttpResponse:
+    try:
+        created_item = Item.objects.create(
+            name=request.POST["item_name"],
+            size_grams=request.POST["size_grams"],
+        )
+    except ValueError as e:
+        return render(request, "editor.html", {"error_message": e})
+    return HttpResponseRedirect(reverse("manager:detail", args=(created_item.id,)))
