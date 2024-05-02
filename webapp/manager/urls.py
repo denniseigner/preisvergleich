3cdf9b6ca81a26ff2cbb54ca69a80fd58c8aca1d
Dennis Eigner <dennis@deigner.eu>
2024-05-02 17:12:24 +0200

    Add rudimentary views

--- webapp/manager/urls.py
+++ webapp/manager/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "manager"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("items/<int:item_id>/", views.detail, name="detail"),
 ]
--- webapp/manager/views.py
+++ webapp/manager/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Item
 
 
-def index() -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request: HttpRequest) -> HttpResponse:
+    item_list = Item.objects.all()
+    context = {"item_list": item_list}
+    return render(request, "index.html", context)
+
+
+def detail(request: HttpRequest, item_id: int) -> HttpResponse:
+    item = get_object_or_404(Item, pk=item_id)
+    return render(request, "detail.html", {"item": item})
