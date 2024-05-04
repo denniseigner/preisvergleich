7bdefb2d54ee85ed6da883bafd8ac80b9d9dbf73
Dennis Eigner <dennis@deigner.eu>
2024-05-04 13:08:11 +0200

    Add model for editor

--- manager/urls.py
+++ manager/urls.py
@@ -5,7 +5,4 @@ from . import views
 app_name = "manager"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("items/<int:item_id>/", views.detail, name="detail"),
-    path("editor", views.editor, name="editor"),
-    path("create_new", views.create_new, name="create_new"),
 ]
